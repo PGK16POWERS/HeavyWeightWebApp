d8452a381ea0eb0f24b24f23d971943a1c0a3a53
programmer Kartier <progrmmerKartier@gmail.com>
2025-02-24 08:49:37 +0200

    ts

--- heavy-weight/src/app/app.module.ts
+++ heavy-weight/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicWebPageComponent } from './public-web-page/public-web-page.component';
+import { SecurityComponent } from './security/security.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicWebPageComponent,
+    SecurityComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
---
+++ heavy-weight/src/app/public-web-page/public-web-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-web-page',
+  standalone: false,
+  templateUrl: './public-web-page.component.html',
+  styleUrl: './public-web-page.component.css'
+})
+export class PublicWebPageComponent {
+
+}
