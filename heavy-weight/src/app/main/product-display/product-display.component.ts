c575b3a47725c5438978d946b1cdd5ecbb6e5662
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2025-04-25 17:50:19 +0200

    tough

--- heavy-weight/src/app/main/product-display/product-display.component.ts
+++ heavy-weight/src/app/main/product-display/product-display.component.ts
@@ -13,5 +13,38 @@ export class ProductDisplayComponent implements OnInit {
       const bodyHeight = document.body.clientHeight;
       const mainAppSection = document.querySelector("#main-app-section") as HTMLElement;
       mainAppSection.style.minHeight = bodyHeight + "px"; 
+
+      // SHOW AND HIDE OPERATIONS
+      const showDescIcon = document.querySelector("#show-description") as HTMLElement;
+      const hideDescIcon = document.querySelector("#hide-description") as HTMLElement;
+      const descInfo = document.querySelector("#description-info") as HTMLElement;
+
+      showDescIcon.addEventListener("click", () => {
+        if (hideDescIcon.style.display = "none") {
+          showDescIcon.style.display = "none";
+          hideDescIcon.style.display = "flex";
+          descInfo.style.display = "flex";
+        }
+      });
+
+      hideDescIcon.addEventListener("click", () => {
+        if (showDescIcon.style.display = "none") {
+          showDescIcon.style.display = "flex";
+          hideDescIcon.style.display = "none";
+          descInfo.style.display = "none";
+        }
+      });
+
+      const showDetIcon = document.querySelector("#show-details") as HTMLElement;
+      const hideDetcIcon = document.querySelector("#hide-details") as HTMLElement;
+      const detInfo = document.querySelector("#details-info") as HTMLElement;
+
+      showDetIcon.addEventListener("click", () => {
+        if (hideDetcIcon.style.display = "none") {
+          showDetIcon.style.display = "none";
+          hideDetcIcon.style.display = "flex";
+          detInfo.style.display = "flex";
+        }
+      });
   }
 }
