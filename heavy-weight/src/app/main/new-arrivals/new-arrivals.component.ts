07cb7446d3d2fc613fe2a654053caf5ccddf9802
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2025-04-25 11:58:02 +0200

    ts

--- heavy-weight/src/app/main/new-arrivals/new-arrivals.component.ts
+++ heavy-weight/src/app/main/new-arrivals/new-arrivals.component.ts
@@ -66,6 +66,24 @@ export class NewArrivalsComponent implements OnInit, AfterViewInit {
       const mainAppSection = document.querySelector("#main-app-section") as HTMLElement;
       mainAppSection.style.minHeight = bodyHeight + "px";     
 
+      // SHOW MENU BARS
+    const menuBars = document.querySelector("#menu-bar") as HTMLElement;
+    const floatingMenu = document.querySelector("#floating-div") as HTMLElement;
+
+    menuBars.addEventListener("click", (e) => {
+      e.stopPropagation(); // Prevent it from triggering the body click
+      floatingMenu.classList.add("active");
+      document.body.style.position = "fixed";
+    });
+    
+    // Close the menu when clicking outside
+    document.addEventListener("click", (event) => {
+      if (!floatingMenu.contains(event.target as Node) && event.target !== menuBars) {
+        floatingMenu.classList.remove("active");
+        document.body.style.position = "";
+      }
+    });
+
   }
 
   ngAfterViewInit(): void {
