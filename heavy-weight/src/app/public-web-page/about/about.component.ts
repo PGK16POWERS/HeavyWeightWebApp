0a4e156417e98abd8c3c2767e1e4857f5d34026e
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2025-04-25 11:58:47 +0200

    ts

--- heavy-weight/src/app/public-web-page/about/about.component.ts
+++ heavy-weight/src/app/public-web-page/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  ngOnInit(): void {
+
+    // SHOW MENU BARS
+    const menuBars = document.querySelector("#menu-bar") as HTMLElement;
+    const floatingMenu = document.querySelector("#floating-div") as HTMLElement;
+
+    menuBars.addEventListener("click", (e) => {
+      e.stopPropagation(); // Prevent it from triggering the body click
+      floatingMenu.classList.add("active");
+      document.body.style.position = "fixed";
+    });
+    
+    // Close the menu when clicking outside
+    document.addEventListener("click", (event) => {
+      if (!floatingMenu.contains(event.target as Node) && event.target !== menuBars) {
+        floatingMenu.classList.remove("active");
+        document.body.style.position = "";
+      }
+    });
+    
+  }
 }
--- heavy-weight/src/app/public-web-page/public-web-page.component.ts
+++ heavy-weight/src/app/public-web-page/public-web-page.component.ts
@@ -13,7 +13,7 @@ export class PublicWebPageComponent implements OnInit {
   constructor(private router: Router) {}
 
   newsletterModel = {
-    email: ''
+    email: ""
   }
 
   ngOnInit(): void {
@@ -61,10 +61,19 @@ export class PublicWebPageComponent implements OnInit {
       const menuBars = document.querySelector("#menu-bar") as HTMLElement;
       const floatingMenu = document.querySelector("#floating-div") as HTMLElement;
 
-      menuBars.addEventListener("click", () => {
-        floatingMenu.style.display = "flex"
-        document.body.style.position = "fixed"
-      })
+      menuBars.addEventListener("click", (e) => {
+        e.stopPropagation(); // Prevent it from triggering the body click
+        floatingMenu.classList.add("active");
+        document.body.style.position = "fixed";
+      });
+      
+      // Close the menu when clicking outside
+      document.addEventListener("click", (event) => {
+        if (!floatingMenu.contains(event.target as Node) && event.target !== menuBars) {
+          floatingMenu.classList.remove("active");
+          document.body.style.position = "";
+        }
+      });
   }
 
   async subscribeNewsletter() {
